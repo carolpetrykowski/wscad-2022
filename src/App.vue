d638c3b88eea0c35044502e9babf9a5ef887e292
Caroline Martins Alves <carol.petrykowski@hotmail.com>
2022-03-03 15:05:58 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header />
+    <v-main>
+      <Home />
+      <Footer />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './styles/global.css'
 
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {Header, Home, Footer},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-col class="d-flex align-center flex-column">
+      <v-row>
+        <v-col class="d-flex flex-column align-center" lg="3">
+          <v-row class="flex-grow-0 text-h5 roboto-mono">REALIZAÇÃO</v-row>
+          <img id="logo" src="./../assets/sbc.svg" />
+        </v-col>
+        <v-spacer />
+        <v-col class="d-flex flex-column align-center" lg="3">
+          <v-row class="flex-grow-0 text-h5 roboto-mono">ORGANIZAÇÃO</v-row>
+          <img id="logo" src="./../assets/sbc.svg" />
+        </v-col>
+        <v-spacer />
+        <v-col class="d-flex flex-column align-center" lg="3">
+          <v-row class="flex-grow-0 text-h5 roboto-mono">PATROCINIO</v-row>
+          <img id="logo" src="./../assets/sbc.svg" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+  #logo {
+    width: 200px;
+  }
+</style>
